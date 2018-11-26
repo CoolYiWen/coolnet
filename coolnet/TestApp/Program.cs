9bdaabe1214e557ce8bdcae31ca1cc6162051729
coolyiwen <469810443@qq.com>
2018-11-26 21:14:00 +0800

    first

---
+++ coolnet/TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CoolNet;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Class1();
+            while (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ coolnet/coolnet/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoolNet
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("这是一个类库");
+        }
+    }
+}
